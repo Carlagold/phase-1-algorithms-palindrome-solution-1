33da6e7cb1bb88a4202f17b723e93b0f08d07c43
Carla <csachett@student.scf.edu>
2022-08-10 00:41:32 -0400

    Completed assignement

--- index.js
+++ index.js
@@ -1,20 +1,62 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function reverse(word) {
+  // 'abc' => 'cba'
+//   const wordArray = word.split("");
+//   const reversedWordArray = wordArray.reverse();
+//   const reverseWord = reversedWordArray.join("");
+//   return reverseWord;
+// LOL I did this solution on my first Palindrome LAB
+return word.split("").reverse().join(""); 
 }
 
+function isPalindrome(word) {
+  // Write your algorithm here
+  // reverse the input string
+  const reversedWord = reverse(word)
+  // if the input word is the same to the transposed word
+  return word === reversedWord
+  // if (word === reversedWord) {
+  //   return true
+  // } else {
+  //   return false
+  // }
+}
+
+
 /* 
+/* Test Cases for validation of undertanding
+mom === mom // true
+car === rac //false
+*/
+
+/* Rewrite the problem in your own words
   Add your pseudocode here
+  make function that returns true if a word is a palindrome and false if not.
+  if the input word is the same to the transposed word, the return should be true.
 */
-
+/* Test Case
+mom === mom // true
+car === rac //false
+*/
 /*
-  Add written explanation of your solution here
-*/
+  Add written explanation of your solution here:
 
-// You can run `node index.js` to view these console logs
+
+*/
+/* add pseudocode here
+if the input word is the same to the transposed word, the return should be true.
+// reverse the input string
+if the input is the same as the trasposed input 
+ return true
+ else 
+  return false
+*/
+  // You can run `node index.js` to view these console logs
+
 if (require.main === module) {
-  // add your own custom tests in here
+  
+//  add your own custom tests in here
   console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+  console.log("=>", isPalindrome("abba"));
 
   console.log("");
 
